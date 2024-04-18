7ef58fbc8732b9fe8a66a3357706de3b80e62ee1
rzv <51793648+RazviAlex@users.noreply.github.com>
2024-04-18 17:03:44 +0200

    Update loadGPT.py

--- loadGPT.py
+++ loadGPT.py
@@ -20,7 +20,7 @@ def callGPT(event, args):
 
   client = OpenAI()
 
-  if modelArguments == "GPT-3.5T":    
+  if modelArguments == "gpt-3.5T":    
     print("\n\nChatGPT-3.5 Turbo analysis...")
     completion = client.chat.completions.create(
       model="gpt-3.5-turbo-1106",
@@ -30,20 +30,29 @@ def callGPT(event, args):
         {"role": "user", "content": "A little bit of arithmetic and a logical approach will help us quickly arrive at the solution to this problem. Analyse each event (one by one and then together) for any observed patterns or anomalies. Answer must be structured as follows: A single paragraph that contains a final conclusion. The conclusion starts with a verdict (malicious (100% sure is a malicious behaviour), suspicious (you are not sure) or benign(you are 100% sure is not a malicious behaviour) ), followed by a short explanation of why this verdict. Finalize with an 'Extra part' where focus on the two most interesting data extracted from events (write the value of those events) that may be anomalous or suspicious."}
       ]
     )
-  elif modelArguments == "GPT-4":
+  elif modelArguments == "gpt-4-32k":
     print("\n\nChatGPT-4 analysis...")
     completion = client.chat.completions.create(
-      model="gpt-4",
+      model="gpt-4-32k-0314",
       temperature=0.3,
       messages=[
         {"role": "system", "content": "Given the following set of cybersecurity events which are structed as follows: EventType -> endpoint -> SourceParentProcess -> SourceProcess, etc, your job is to analyse step by step and understand what type of activity has been carried out. These are the events: "+str(event)},
         {"role": "user", "content": "Let’s combine our numerical command and clear thinking to quickly and accurately decipher the answer. Answer must be structured as follows: A single paragraph that contains a final conclusion. The conclusion starts with a verdict (malicious (100% sure is a malicious behaviour), suspicious (you are not sure) or benign(you are 100% sure is not a malicious behaviour) ), followed by an explanation of why this verdict. Finalize with an 'Extra part' where focus on the two most interesting data extracted from events (write the value of those events) that may be anomalous or suspicious."}
       ]
     )
+  elif modelArguments == "gpt-4-preview":
+    print("\n\nChatGPT-4 analysis...")
+    completion = client.chat.completions.create(
+      model="gpt-4-0125-preview",
+      temperature=0.3,
+      messages=[
+        {"role": "system", "content": "Given the following set of cybersecurity events which are structed as follows: EventType -> endpoint -> SourceParentProcess -> SourceProcess, etc, your job is to analyse step by step and understand what type of activity has been carried out. These are the events: "+str(event)},
+        {"role": "user", "content": "Let’s combine our numerical command and clear thinking to quickly and accurately decipher the answer. Answer must be structured as follows: A single paragraph that contains a final conclusion. The conclusion starts with a verdict (malicious (100% sure is a malicious behaviour), suspicious (you are not sure) or benign(you are 100% sure is not a malicious behaviour) ), followed by an explanation of why this verdict. Finalize with an 'Extra part' where focus on the two most interesting data extracted from events (write the value of those events) that may be anomalous or suspicious."}
+      ]
+    )    
 
   print(completion.choices[0].message.content)
   #print(completion.choices[1].message.content)
-
 
 def exportS1Data(args):
   event = []
